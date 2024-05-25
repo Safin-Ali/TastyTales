7dd0f5668d9c407176da2abe1e52fc8b6584e834
Safin-Ali <safin.ali.7205@gmail.com>
2024-05-25 15:09:05 +0600

    style: :lipstick: layout main section rounded

--- src/Layout/RootLayout.tsx
+++ src/Layout/RootLayout.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import AppBar from '../components/AppBar/AppBar';
 
 const RootLayout: React.FC = () => {
 
 	return (
 		<>
-			<main className={`min-h-[calc(100dvh-20dvh)]`}>
+			<AppBar/>
+			<main
+				className={`min-h-[calc(100dvh-20dvh)] bg-white container my-5 rounded-2xl mx-auto`}
+			>
 				<Outlet />
 			</main>
 		</>
